73df8ca254cf46383954a96f9b11a5e7ba86789d
Yurii Reshetylo <yurii.reshetylo@oracle.com>
2017-05-27 22:53:44 -0600

    auth module

---
+++ src/admintools/auth.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/gorilla/sessions"
+)
+
+var sessionStore *sessions.CookieStore
+
+func startSessions() {
+	sessionStore = sessions.NewCookieStore([]byte(config.CookieSecret))
+
+}
